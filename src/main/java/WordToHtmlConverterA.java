7f4aaa0e2a0e9736b8d284c8996bfbf950234fe1
alakey <prosoulk2017@gmail.com>
2023-04-14 12:55:27 +0800

    add convert docx -> pdf -> html

--- src/main/java/WordToHtmlConverterA.java
+++ src/main/java/WordToHtmlConverterA.java
@@ -2,24 +2,95 @@ import java.io.*;
 
 import fr.opensagres.poi.xwpf.converter.core.ImageManager;
 import fr.opensagres.poi.xwpf.converter.core.XWPFConverterException;
+import fr.opensagres.poi.xwpf.converter.pdf.PdfConverter;
+import fr.opensagres.poi.xwpf.converter.pdf.PdfOptions;
 import fr.opensagres.poi.xwpf.converter.xhtml.XHTMLConverter;
 import fr.opensagres.poi.xwpf.converter.xhtml.XHTMLOptions;
+import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.poi.hwpf.HWPFDocument;
 import org.apache.poi.hwpf.converter.PicturesManager;
 import org.apache.poi.hwpf.converter.WordToHtmlConverter;
 import org.apache.poi.hwpf.usermodel.*;
-import org.apache.poi.xwpf.usermodel.*;
+
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFTable;
+import org.apache.poi.xwpf.usermodel.XWPFTableCell;
+import org.apache.poi.xwpf.usermodel.XWPFTableRow;
+import org.fit.pdfdom.PDFDomTree;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class WordToHtmlConverterA {
 
     public static void main(String[] args) throws Throwable {
-        convertDocx("input.docx");
+        //convertDoc("input.docx");
+        //convertDocx("input.docx");
+        //convertAddImgTag("input.docx");
+        convertDocxToHtml("input.docx");
+    }
+
+    //docx -> pdf -> html
+    private static void convertDocxToHtml(String fileName) throws IOException {
+        //pdf
+        String docPath = fileName;
+        String pdfPath = "output.pdf";
+
+        InputStream in = new FileInputStream(new File(docPath));
+        XWPFDocument document = new XWPFDocument(in);
+        PdfOptions options = PdfOptions.create();
+        OutputStream out = new FileOutputStream(new File(pdfPath));
+        PdfConverter.getInstance().convert(document, out, options);
+
+        document.close();
+        out.close();
+
+        //html
+        PDDocument pdf = PDDocument.load(new File(pdfPath));
+        Writer output = new PrintWriter("output.html", "utf-8");
+        new PDFDomTree().writeText(pdf, output);
+        output.close();
+        System.out.println("Done");
+    }
+
+    private static void convertAddImgTag(String fileName) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(new File(fileName));
+        XWPFDocument document = new XWPFDocument(fileInputStream);
+
+        XHTMLOptions options = XHTMLOptions.create();
+        options.setImageManager(new ImageManager(new File("./"), "images"));
+
+        for (XWPFTable table : document.getTables()) {
+            for (XWPFTableRow row : table.getRows()) {
+                for (XWPFTableCell cell : row.getTableCells()) {
+                    if (cell.getText().trim().isEmpty()) {
+                        cell.setText(" ");
+                    }
+                }
+            }
+        }
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        XHTMLConverter.getInstance().convert(document, outputStream, options);
+        String html = outputStream.toString();
+
+        Document doc = Jsoup.parse(html);
+        Elements imgs = doc.select("img");
+        for (Element img : imgs) {
+            img.attr("style", "float: left; margin-right: 10px;");
+        }
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter("output.html"));
+        writer.write(doc.outerHtml());
+        writer.close();
+        System.out.println("Done");
     }
 
     private static void convertDocx(String fileName) {
@@ -28,7 +99,8 @@ public class WordToHtmlConverterA {
             FileInputStream fis = new FileInputStream(fileName);
             XWPFDocument document = new XWPFDocument(fis);
             FileOutputStream fos = new FileOutputStream(outputFilePath);
-            XHTMLOptions options = XHTMLOptions.create().setImageManager(new ImageManager(new File("./"), "images"));
+            XHTMLOptions options = XHTMLOptions.create();
+            options.setImageManager(new ImageManager(new File("./"), "images"));
 
             for (XWPFTable table : document.getTables()) {
                 for (XWPFTableRow row : table.getRows()) {
@@ -44,6 +116,7 @@ public class WordToHtmlConverterA {
 
             fis.close();
             fos.close();
+
             System.out.println("Done");
         } catch (XWPFConverterException e) {
             System.out.println("Error");
